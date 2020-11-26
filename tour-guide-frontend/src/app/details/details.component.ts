b5858785e0630331d800a66b01606d16573dd468
Arjunadappa <arjunadappa007@gmail.com>
2020-11-26 19:51:13 +0530

    map-box targets implemented

--- tour-guide-frontend/src/app/details/details.component.ts
+++ tour-guide-frontend/src/app/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpserviceService} from "../httpservice.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-details',
@@ -10,6 +11,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class DetailsComponent implements OnInit {
   userdetails;
   tour
+  map: mapboxgl.Map;
   constructor(private router: Router, private route: ActivatedRoute,private httpService: HttpserviceService) { }
 
   ngOnInit(): void {
@@ -19,8 +21,30 @@ export class DetailsComponent implements OnInit {
     })
     this.httpService.getTour(this.route.snapshot.paramMap.get('id')).subscribe(tour => {
       this.tour = tour;
-      console.log(tour)
+      console.log(tour);
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        accessToken:'pk.eyJ1IjoidHVzaGFyZGciLCJhIjoiY2toeDl6OHFqMjRmZTJycGJ5ZTh0ZnB6eiJ9.BTWJHE9O_pIK9Nm7b1o-fg',
+        style: 'mapbox://styles/mapbox/dark-v10',
+        zoom: 13,
+        center: tour.data.data.startLocation.coordinates
+      });
+      let marker = new mapboxgl.Marker()
+        .setLngLat(tour.data.data.startLocation.coordinates)
+        .addTo(this.map);
+      if(tour.data.data.locations.length > 0){
+        tour.data.data.locations.forEach((location) => {
+          let marker = new mapboxgl.Marker()
+            .setLngLat(location.coordinates)
+            .addTo(this.map);
+        })
+      }
     })
+    //mapboxgl.accessToken = 'pk.eyJ1IjoidHVzaGFyZGciLCJhIjoiY2toeDl6OHFqMjRmZTJycGJ5ZTh0ZnB6eiJ9.BTWJHE9O_pIK9Nm7b1o-fg';
+
+    let marker = new mapboxgl.Marker()
+      .setLngLat([12.550343, 55.665957])
+      .addTo(this.map);
   }
   logout() {
     this.httpService.logout().subscribe((data) => {
