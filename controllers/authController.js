189f4db4488ea57794214482f28c39801436d0a5
arjun <arjunadappa007@gmail.com>
2020-11-28 22:39:02 +0530

    bug fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,6 +19,7 @@ const createSendToken = (user, statusCode, req, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
+    domain: 'http://localhost:4200',
     httpOnly: true,
     sameSite: 'Lax',
     secure: true
